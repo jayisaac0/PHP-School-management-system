4f522b26df4ee1b9b6144a1cd46106cb8503e804
Joshua Isaac <jayisaac0@gmail.com>
2019-08-02 08:14:42 +0300

    Update toppanelnav.php

--- includes/nav/toppanelnav.php
+++ includes/nav/toppanelnav.php
@@ -5,25 +5,6 @@
 			<div class="nav toggle">
 				<a id="menu_toggle"><i class="fa fa-bars"></i></a>
 			</div>
-			<ul class="nav navbar-nav navbar-right">
-				<li class="">
-					<a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-						<img src="images/img.jpg" alt="">John Doe
-						<span class=" fa fa-angle-down"></span>
-					</a>
-					<ul class="dropdown-menu dropdown-usermenu pull-right">
-						<li><a href="javascript:;"> Profile</a></li>
-						<li>
-							<a href="javascript:;">
-								<span class="badge bg-red pull-right">50%</span>
-								<span>Settings</span>
-							</a>
-						</li>
-						<li><a id="compose">Change password</a></li>
-					</ul>
-				</li>
-				
-			</ul>
 		</nav>
 	</div>
 </div>
